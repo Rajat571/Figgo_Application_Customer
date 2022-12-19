adf24f3294b5f85f316b26870ff719c6e08f7423
Jagparvesh <jagparvesh@pearlorganisation.com>
2022-12-19 11:08:46 +0530

    Jagparvesh

--- app/src/main/java/com/pearlorganisation/figgo/UI/Fragments/Advance_cityCab.kt
+++ app/src/main/java/com/pearlorganisation/figgo/UI/Fragments/Advance_cityCab.kt
@@ -1,21 +1,31 @@
 package com.pearlorganisation.figgo.UI.Fragments
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.pearlorganisation.figgo.Adapter.AdvanceCityAdapter
 import com.pearlorganisation.figgo.Model.AdvanceCityCab
 import com.pearlorganisation.figgo.R
 import com.pearlorganisation.figgo.databinding.FragmentAdvanceCityCabBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class Advance_cityCab : Fragment() {
     lateinit var binding: FragmentAdvanceCityCabBinding
     lateinit var advanceCityAdapter: AdvanceCityAdapter
     var cablist=ArrayList<AdvanceCityCab>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +35,49 @@ class Advance_cityCab : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var calenderimg = view.findViewById<ImageView>(R.id.calenderimg)
+        var datetext = view.findViewById<TextView>(R.id.datetext)
+        var watchimg = view?.findViewById<ImageView>(R.id.watchimg)
+        var timetext = view?.findViewById<TextView>(R.id.timetext)
+        calenderimg.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = context?.let { it1 ->
+                DatePickerDialog(
+                    it1,
+                    { view, year, monthOfYear, dayOfMonth ->
+
+                        val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                        datetext.setText(dat)
+                    },
+                    year,
+                    month,
+                    day
+                )
+            }
+
+            if (datePickerDialog != null) {
+                datePickerDialog.show()
+            }
+            watchimg?.setOnClickListener {
+                val cal = Calendar.getInstance()
+                val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                    cal.set(Calendar.HOUR_OF_DAY, hour)
+                    cal.set(Calendar.MINUTE, minute)
+                    if (timetext != null) {
+                        timetext.text = SimpleDateFormat("HH:mm").format(cal.time)
+                    }
+                }
+                TimePickerDialog(context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+            }
+
+
+        }
+
         binding.recylerCabList.layoutManager=GridLayoutManager(context,4)
         cablist.add(AdvanceCityCab(R.drawable.figgo_auto,"75-100"))
         cablist.add(AdvanceCityCab(R.drawable.figgo_bike,"45-65"))
@@ -40,4 +92,5 @@ class Advance_cityCab : Fragment() {
         advanceCityAdapter=AdvanceCityAdapter(requireActivity(),cablist)
         binding.recylerCabList.adapter=advanceCityAdapter
     }
+
 }
