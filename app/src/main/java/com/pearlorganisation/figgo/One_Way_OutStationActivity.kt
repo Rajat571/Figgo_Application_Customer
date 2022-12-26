e9d0bf580bdb87f510295af7ee36c6cef24b8767
Jagparvesh <jagparvesh@pearlorganisation.com>
2022-12-26 11:06:33 +0530

    Jagparvesh

---
+++ app/src/main/java/com/pearlorganisation/figgo/One_Way_OutStationActivity.kt
@@ -0,0 +1,27 @@
+package com.pearlorganisation.figgo
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class One_Way_OutStationActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_way_out_station)
+        var window=window
+        window.setStatusBarColor(Color.parseColor("#000F3B"))
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController=navHostFragment.navController
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.navigation_bar)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+
+    }
+}
