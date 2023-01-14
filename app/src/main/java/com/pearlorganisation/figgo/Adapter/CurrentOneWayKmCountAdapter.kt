9abc7a49a2e68efe9a64396f3b13b93d4b7bad4d
Jagparvesh <jagparvesh@pearlorganisation.com>
2023-01-14 15:52:05 +0530

    Jagparvesh

--- app/src/main/java/com/pearlorganisation/figgo/Adapter/CurrentOneWayKmCountAdapter.kt
+++ app/src/main/java/com/pearlorganisation/figgo/Adapter/CurrentOneWayKmCountAdapter.kt
@@ -2,6 +2,7 @@ package com.pearlorganisation.figgo.Adapter
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,19 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.AuthFailureError
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.pearlorganisation.PrefManager
 import com.pearlorganisation.figgo.CurrentMap.EmergencyMapsActivity
 import com.pearlorganisation.figgo.CurrentMap.MapsActivity1
 import com.pearlorganisation.figgo.CurrentMap.MapsActivity2
 import com.pearlorganisation.figgo.Model.OneWayListRatingVehicle
 import com.pearlorganisation.figgo.R
+import org.json.JSONObject
 
 class CurrentOneWayKmCountAdapter(var context: Context, private val mList: List<OneWayListRatingVehicle>) : RecyclerView.Adapter<CurrentOneWayKmCountAdapter.ViewHolder>() {
 
@@ -44,7 +52,52 @@ class CurrentOneWayKmCountAdapter(var context: Context, private val mList: List<
 
         holder.acceptbutton.setOnClickListener {
             Toast.makeText(context,"Accepted Driver",Toast.LENGTH_SHORT).show()
-            context.startActivity(Intent(context, EmergencyMapsActivity::class.java))
+
+            val URL = "https://test.pearl-developer.com/figo/api/ride/select-driver"
+           // Log.d("SendData", "URL===" + URL)
+            val queue = Volley.newRequestQueue(context)
+            val json = JSONObject()
+            json.put("ride_id",pref.getride_id())
+            json.put("driver_id",pref.getdriver_id())
+            json.put("price",pref.getprice())
+            Log.d("SendData", "pref.getToken()===" + pref.getToken())
+            Log.d("SendData", "json===" + json)
+            val jsonOblect: JsonObjectRequest = object : JsonObjectRequest(Method.POST, URL, json, object :
+                Response.Listener<JSONObject?>               {
+                override fun onResponse(response: JSONObject?) {
+                    Log.d("SendData", "response===" + response)
+                    if (response != null) {
+                        val status = response.getString("status")
+                        if(status.equals("false")){
+                            Toast.makeText(context, "Something Went Wrong!", Toast.LENGTH_LONG).show()
+                        }else{
+
+
+
+                        }
+                    }
+
+                }
+            }, object : Response.ErrorListener {
+                override fun onErrorResponse(error: VolleyError?) {
+                    Log.d("SendData", "error===" + error)
+                    Toast.makeText(context, "Something Went Wrong!", Toast.LENGTH_LONG).show()
+                }
+            }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers: MutableMap<String, String> = java.util.HashMap()
+                    headers.put("Content-Type", "application/json; charset=UTF-8")
+                    headers.put("Authorization", "Bearer " + pref.getToken())
+                    return headers
+                }
+            }
+            jsonOblect.setRetryPolicy(DefaultRetryPolicy(10000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT))
+
+            queue.add(jsonOblect)
+
+
+        context.startActivity(Intent(context, EmergencyMapsActivity::class.java))
 
 
         }
--- app/src/main/java/com/pearlorganisation/figgo/CurrentMap/MapsActivity2.kt
+++ app/src/main/java/com/pearlorganisation/figgo/CurrentMap/MapsActivity2.kt
@@ -66,18 +66,18 @@ class MapsActivity2 : BaseClass(), OnMapReadyCallback, GoogleMap.OnMarkerClickLi
         super.onCreate(savedInstanceState)
         binding = ActivityMaps2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        pref  = PrefManager(this)
+        pref = PrefManager(this)
         shareimg()
         onBackPress()
 
         var accept = findViewById<TextView>(R.id.accept)
 
-         activaimg = findViewById<ImageView>(R.id.activaimg)
-         activavehiclenumber = findViewById<TextView>(R.id.activavehiclenumber)
-         drivername = findViewById<TextView>(R.id.drivername)
+        activaimg = findViewById<ImageView>(R.id.activaimg)
+        activavehiclenumber = findViewById<TextView>(R.id.activavehiclenumber)
+        drivername = findViewById<TextView>(R.id.drivername)
 
-         ride_service_rating = findViewById<RatingBar>(R.id.ride_service_rating)
-         dl_number = findViewById<TextView>(R.id.dl_number)
+        ride_service_rating = findViewById<RatingBar>(R.id.ride_service_rating)
+        dl_number = findViewById<TextView>(R.id.dl_number)
         driverimg = findViewById<ImageView>(R.id.driverimg)
 
 
@@ -91,55 +91,13 @@ class MapsActivity2 : BaseClass(), OnMapReadyCallback, GoogleMap.OnMarkerClickLi
             val intent = Intent(this, EmergencyMapsActivity::class.java)
             intent.putExtras(bundle)
             startActivity(intent)
-                val URL = "https://test.pearl-developer.com/figo/api/ride/select-driver"
-                Log.d("SendData", "URL===" + URL)
-                val queue = Volley.newRequestQueue(this)
-                val json = JSONObject()
-                json.put("ride_id",pref.getride_id())
-                json.put("driver_id",pref.getdriver_id())
-                json.put("price",pref.getprice())
-                Log.d("SendData", "pref.getToken()===" + pref.getToken())
-                Log.d("SendData", "json===" + json)
-                val jsonOblect: JsonObjectRequest = object : JsonObjectRequest(Method.POST, URL, json, object :
-                    Response.Listener<JSONObject?>               {
-                    override fun onResponse(response: JSONObject?) {
-                        Log.d("SendData", "response===" + response)
-                        if (response != null) {
-                            val status = response.getString("status")
-                            if(status.equals("false")){
-                                Toast.makeText(this@MapsActivity2, "Something Went Wrong!", Toast.LENGTH_LONG).show()
-                            }else{
 
 
-                            }
-                        }
-
-                    }
-                }, object : Response.ErrorListener {
-                    override fun onErrorResponse(error: VolleyError?) {
-                        Log.d("SendData", "error===" + error)
-                        Toast.makeText(this@MapsActivity2, "Something Went Wrong!", Toast.LENGTH_LONG).show()
-                    }
-                }) {
-                    @Throws(AuthFailureError::class)
-                    override fun getHeaders(): Map<String, String> {
-                        val headers: MutableMap<String, String> = java.util.HashMap()
-                        headers.put("Content-Type", "application/json; charset=UTF-8")
-                        headers.put("Authorization", "Bearer " + pref.getToken())
-                        return headers
-                    }
-                }
-                jsonOblect.setRetryPolicy(DefaultRetryPolicy(10000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT))
-
-                queue.add(jsonOblect)
-
-            }
-
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
     }
 
 
